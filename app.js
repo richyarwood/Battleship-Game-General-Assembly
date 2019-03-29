8e48b08611dacaeb11f571db3bb12e6b8a65ac8d
Richard Yarwood <richyarwood@gmail.com>
2019-03-29 10:03:45 +0000

    Computer can choose a random square

--- app.js
+++ app.js
@@ -18,17 +18,25 @@ Horizontal: ((index + ship.size)) / width <= 1
 
 */
 
-let width = 10
+  let width = 10
 
-function createGrid() {
+  function createGrid() {
     for (let i = 0; i < (width * width); i++) {
       const square = document.createElement('div')
       document.querySelector('.grid').appendChild(square)
       square.classList.add('square')
     }
-}
+  }
 
-createGrid()
+  function computerPlaceShips() {
+    const squares = document.querySelectorAll('.square')
+    console.log(squares)
+    const computerChoice = squares[Math.floor(Math.random() * squares.length)]
+    computerChoice.classList.add('ship-square')
+  }
+
+  createGrid()
+  computerPlaceShips()
 
 
 
