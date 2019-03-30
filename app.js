1874865916b00e7764717f9fe535bb7641b66f48
Richard Yarwood <richyarwood@gmail.com>
2019-03-30 12:36:44 +0000

    Blockade finished

--- app.js
+++ app.js
@@ -20,54 +20,49 @@ document.addEventListener('DOMContentLoaded', () =>{
   }
 
   function computerPlaceShips() {
-    // Choose horizontal or vertical ship
-    let randomDirection = Math.random() >= 0.5
-    randomDirection = false
-    console.log(randomDirection)
 
-    randomIndex = Math.floor(Math.random() * squares.length)
-    randomIndex = 4
+    for (let i = 0; i < numShips; i++){
+      // Choose horizontal or vertical ship
+      let randomDirection = Math.random() >= 0.5
+      randomIndex = Math.floor(Math.random() * squares.length)
 
-    let columnIndex = (randomIndex % width)
-    let rowIndex = Math.floor(randomIndex/width)
-    console.log(randomIndex)
+      let columnIndex = (randomIndex % width)
+      let rowIndex = Math.floor(randomIndex/width)
 
-    //Make horizontal ship
-
-    if (randomDirection === true) {
-      orientation = 1
-      while ((width - columnIndex) < shipLength) {
-        console.log('Different random index')
-        randomIndex = Math.floor(Math.random() * squares.length)
-        columnIndex = (randomIndex % width)
+      //Make horizontal ship
+      if (randomDirection === true) {
+        orientation = 1
+        while ((width - columnIndex) < shipLength) {
+          randomIndex = Math.floor(Math.random() * squares.length)
+          columnIndex = (randomIndex % width)
+        }
+        // Make vertical ship
+      } else {
+        orientation = 10
+        while ((rowIndex - 1 + shipLength) >= width) {
+          randomIndex = Math.floor(Math.random() * squares.length)
+          columnIndex = (randomIndex % width)
+          rowIndex = Math.floor(randomIndex/width)
+        }
       }
-    } else {
-      orientation = 10
-      while ((rowIndex - 1 + shipLength) >= width) {
-        randomIndex = Math.floor(Math.random() * squares.length)
-        columnIndex = (randomIndex % width)
-        rowIndex = Math.floor(randomIndex/width)
+      for (let i = 0; i < shipLength; i++) {
+        const nextVerticalIndex = randomIndex + i * orientation
+        const shipSquare = squares[nextVerticalIndex]
+        shipSquare.classList.add('ship')
       }
+      if (randomDirection === true){
+        blockAroundHorizontalShip()
+      } else
+      blockAroundVerticalShip()
     }
-    for (let i = 0; i < shipLength; i++) {
-      const nextVerticalIndex = randomIndex + i * orientation
-      const shipSquare = squares[nextVerticalIndex]
-      shipSquare.classList.add('ship')
-    }
-
-    // if (randomDirection === true){
-      // blockAroundHorizontalShip()
-    // } else
-    blockAroundVerticalShip()
   }
 
+//CREATES THE BLOCKADE AROUND THE VERTICAL SHIP==============
   function blockAroundVerticalShip() {
     let lengthOfBlockade = shipLength + 2
     const rowIndex = Math.floor(randomIndex / width)
     const columnIndex = (randomIndex % width)
-    console.log(columnIndex, 'Col i')
     let startOfBlockade = (randomIndex - width + 1)
-    console.log(startOfBlockade ,'Initial start')
     let endOfBlockade = (randomIndex - width - 1)
     let topBlockade = (randomIndex - width)
     let bottomBlockade = (randomIndex + (shipLength * width))
@@ -75,12 +70,10 @@ document.addEventListener('DOMContentLoaded', () =>{
 
     // If vertical ship against left
     if (columnIndex === 0 && rowIndex !== 0 && rowIndex !== width - shipLength){
-      console.log('vertical left hand side')
       endOfBlockade = startOfBlockade
 
       // If vertical ship against right
     } else if (columnIndex === 9 && rowIndex !== 0 && rowIndex !== (width - shipLength)){
-      console.log('Vertical right hand side')
       startOfBlockade = endOfBlockade
 
       // If vertical ship on bottom row
@@ -90,7 +83,6 @@ document.addEventListener('DOMContentLoaded', () =>{
 
       // If vertical ship in top left corner
     } else if (randomIndex === 0){
-      console.log('Vertical top left corner')
       startOfBlockade = randomIndex + 1
       endOfBlockade = startOfBlockade
       topBlockade = bottomBlockade
@@ -98,7 +90,6 @@ document.addEventListener('DOMContentLoaded', () =>{
 
       // If vertical ship in the top right corner
     } else if (randomIndex === 9){
-      console.log('top right corner')
       topBlockade = randomIndex + (shipLength * width)
       endOfBlockade = endOfBlockade + width
       startOfBlockade = endOfBlockade
@@ -107,98 +98,80 @@ document.addEventListener('DOMContentLoaded', () =>{
 
       //Vertical ship ends bottom right corner
     } else if (columnIndex === width - 1 && rowIndex === width - shipLength) {
-      console.log('vertical bottom right corner')
       startOfBlockade = endOfBlockade
       bottomBlockade = topBlockade
       lengthOfBlockade--
 
       //Vertical ship ends bottom left corner
     } else if (columnIndex === 0 && rowIndex === width - shipLength){
-      console.log('vertical bottom left')
       endOfBlockade = startOfBlockade
       bottomBlockade = topBlockade
       lengthOfBlockade--
 
       //Vertical ship top row
     } else if (rowIndex === 0 && columnIndex !== 0 && columnIndex !== width - 1){
-      console.log('Vertical top row')
       startOfBlockade = startOfBlockade + width
       endOfBlockade = endOfBlockade + width
       topBlockade = bottomBlockade
       lengthOfBlockade--
     }
 
-      //Basic vertical blockade
-      console.log('Basic vertical blockade')
+      //Default vertical blockade
       squares[topBlockade].classList.add('block') // vertical top middle
-      console.log(topBlockade, 'vertical top')
       squares[bottomBlockade].classList.add('block') // vertical bottom middle
-      console.log(bottomBlockade, 'vertical bottom')
 
       for (let i = 0; i < ((lengthOfBlockade) * width); i = i + 10) {
         squares[startOfBlockade + i].classList.add('block') // vertical right side
-        console.log(startOfBlockade, 'vertical right side')
         squares[endOfBlockade + i].classList.add('block') // vertical left side
-        console.log(endOfBlockade, 'vertical left side')
       }
     }
 
 
-
+//CREATES THE BLOCKADE AROUND THE HORIZONTAL SHIP==============
   function blockAroundHorizontalShip() {
-
     let lengthOfBlockade = shipLength + 2
-
     const rowIndex = Math.floor(randomIndex / width)
     const columnIndex = (randomIndex % width)
-
     let startOfBlockade = (randomIndex - width - 1)
-    console.log(startOfBlockade ,'Initial start')
-
     let endOfBlockade = (randomIndex + width - 1)
-    console.log(endOfBlockade, 'Initial end')
-
     let leftBlockade = randomIndex - 1
-    console.log(leftBlockade, 'left')
-
     let rightBlockade = (randomIndex + shipLength)
-    console.log(rightBlockade, 'right')
+
+    // If ship in top left corner
+    if (randomIndex === 0){
+      endOfBlockade++
+      startOfBlockade = endOfBlockade
+      leftBlockade = rightBlockade
+      lengthOfBlockade--
 
     // If ship is against left side
-    if (randomIndex % width === 0 && columnIndex === 0 & rowIndex !== width - 1){
-      console.log('left side')
+    }else if (randomIndex % width === 0 && columnIndex === 0 & rowIndex !== width - 1){
       startOfBlockade++
       endOfBlockade++
       leftBlockade = rightBlockade
-      console.log(leftBlockade, 'left-hidden')
       lengthOfBlockade--
 
       //If the ship is against right side
     } else if ((randomIndex + shipLength) % width === 0 && rowIndex !== 0 && rowIndex !== width - 1){
       rightBlockade = leftBlockade
-      console.log(rightBlockade, 'right-hidden')
       lengthOfBlockade--
 
       // If ship is against the top
     } else if (rowIndex === 0 && columnIndex !== 0 && ((randomIndex + shipLength) % width > 0)){
-      console.log('top row not corners')
       startOfBlockade = endOfBlockade
 
       //If ship is in top left corner
     } else if (randomIndex === 0) {
-      console.log('top left corner')
       lengthOfBlockade--
 
       //If ship is in top right corner
     } else if (rowIndex === 0){
-      console.log('top right corner end')
       rightBlockade = leftBlockade
       startOfBlockade = endOfBlockade
       lengthOfBlockade--
 
       //If ship is bottom left corner
     } else if (columnIndex === 0 && rowIndex === width - 1) {
-      console.log('bottom left corner')
       startOfBlockade++
       endOfBlockade = startOfBlockade
       leftBlockade = rightBlockade
@@ -206,18 +179,16 @@ document.addEventListener('DOMContentLoaded', () =>{
 
       //Bottom row
     } else if (rowIndex === width - 1 && columnIndex !== 0 && columnIndex !== width - shipLength) {
-      console.log('bottom row')
       endOfBlockade = startOfBlockade
 
       //If ship is bottom right corner
     } else if ((randomIndex + shipLength) % 10 === 0 && rowIndex === width - 1) {
-      console.log('bottom right corner')
       endOfBlockade = startOfBlockade
       rightBlockade = leftBlockade
       lengthOfBlockade--
     }
 
-    // Laying down the blockade
+    // Default blockade
     if (randomIndex === 0) {
       for (let i = 0; i < lengthOfBlockade; i++){
         squares[rightBlockade].classList.add('block') //right
@@ -229,10 +200,7 @@ document.addEventListener('DOMContentLoaded', () =>{
 
       for (let i = 0; i < lengthOfBlockade; i++){
         squares[startOfBlockade + i].classList.add('block') // top
-        console.log(startOfBlockade + i , 'top')
-
         squares[endOfBlockade + i].classList.add('block') // bottom
-        console.log(endOfBlockade, 'bottom')
       }
     }
 
